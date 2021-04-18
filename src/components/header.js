969f5fb1f2301aacd1043507387b79d646a387c3
Masaya Shida <masayashida@gmail.com>
2021-04-18 22:11:22 +0700

    mobile nav configured

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,80 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome,faInfoCircle,faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 import "../styles/styles.css"
 import "../styles/headerStyles.css"
 
-import Logo from "../images/logo.png"
+import LogoWhite from "../images/logo-white.png"
 
+function openNav() {
+  document.getElementById("nav").style.display = "flex"
+  document.getElementById("openNavBtn").style.display = "none"
+  document.getElementById("closeNavBtn").style.display = "block"        
+}
+function closeNav() {
+  document.getElementById("nav").style.display = "none"
+  document.getElementById("closeNavBtn").style.display = "none"
+  document.getElementById("openNavBtn").style.display = "block"
+}
 const Header = ({ siteTitle }) => (
+  
   <header>
-    <Link>
-      <img src={Logo} className="logo"></img>
+    <Link href="/">
+      <img src={LogoWhite} className="logo"></img>
     </Link>
-    <nav>
-      <Link href="/">Home</Link>
-      <Link href="/store">Store</Link>
-      <Link href="/">About</Link>
+    <nav className="desktopNav">
+      <Link href="/">
+        <FontAwesomeIcon 
+          icon={faHome}
+          style={{fontSize: `1.8rem`, maxWidth: `1.8rem`, marginRight: `.8rem`, lineHeight: 0}}
+        />
+        Home
+      </Link>
+      <Link href="/store">
+        <FontAwesomeIcon 
+          icon={faShoppingCart}
+          style={{fontSize: `1.8rem`, maxWidth: `1.8rem`, marginRight: `.8rem`, lineHeight: 0}}
+        />      
+        Store
+      </Link>
+      <Link href="/">
+        <FontAwesomeIcon 
+          icon={faInfoCircle}
+          style={{fontSize: `1.8rem`, maxWidth: `1.8rem`, marginRight: `.8rem`, lineHeight: 0}}
+        />
+        About
+      </Link>
+    </nav>
+    
+    <button id="openNavBtn" onClick={openNav} className="openNavBtn">
+      <span></span>
+      <span></span>
+      <span></span>
+    </button>
+    <button id="closeNavBtn" onClick={closeNav} className="closeNavBtn" style={{display: `none`}}>
+        <div>
+          <span></span>
+          <span></span>
+        </div>
+    </button>
+    <nav class="mobileNav" id="nav">
+      <Link href="/store">
+        <FontAwesomeIcon 
+          icon={faShoppingCart}
+          style={{fontSize: `1.8rem`, maxWidth: `1.8rem`, marginRight: `.8rem`, lineHeight: 0}}
+        />      
+        Store
+      </Link>
+      <Link href="/">
+        <FontAwesomeIcon 
+          icon={faInfoCircle}
+          style={{fontSize: `1.8rem`, maxWidth: `1.8rem`, marginRight: `.8rem`, lineHeight: 0}}
+        />
+        About
+      </Link>
     </nav>
   </header>
 )
